a186a0c620d742cd104cdc3794a94a21400c7dc9
Phil Fevry <phil@auroradigit.al>
2020-03-05 17:33:56 -0800

    Initial Commit

---
+++ MusicWireframe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Phil on 3/5/20.
+//  Copyright © 2020 AURORA Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
